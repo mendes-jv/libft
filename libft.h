ecf362f4a423609c25006028ed7b59542af3ea52
Vinni-Cedraz <Vinni.vcr@gmail.com>
2023-05-07 20:36:48 -0300

    (feat): Implemented isalpha, isalnum, isdigit, tolower and toupper

---
+++ libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jovicto2 <jovicto2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/07 19:07:14 by jovicto2          #+#    #+#             */
+/*   Updated: 2023/05/07 20:12:36 by jovicto2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int	ft_isalpha(int c);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
+int	ft_isalnum(int c);
+int	ft_isdigit(int c);
+
+#endif
