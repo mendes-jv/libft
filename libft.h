812686ccfcc654d9d8185b21718cf7761ef3bfee
João Victor Mendes da Silva <mendes.silva.jv@gmail.com>
2023-05-12 00:44:42 -0300

    (feat): Implemented some functions from part 1

---
+++ libft.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jovicto2 <jovicto2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/09 17:54:09 by jovicto2          #+#    #+#             */
+/*   Updated: 2023/05/10 22:55:16 by jovicto2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_islower(int c);
+int		ft_isprint(int c);
+int		ft_isupper(int c);
+void	*memset(void *s, int c, size_t n);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strrchr(const char *s, int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+
+#endif
---
+++ main.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jovicto2 <jovicto2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/07 19:31:05 by jovicto2          #+#    #+#             */
+/*   Updated: 2023/05/07 22:41:38 by jovicto2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("%i", ft_isalnum('a'));
+	printf("%i", ft_isalnum('4'));
+	printf("%i", ft_isalnum('\n'));
+	printf("%s", "\n");
+	printf("%i", ft_isalpha('a'));
+	printf("%i", ft_isalpha('4'));
+	printf("%i", ft_isalpha('\n'));
+	printf("%s", "\n");
+	printf("%i", ft_isdigit('b'));
+	printf("%i", ft_isdigit('3'));
+	printf("%i", ft_isdigit('\n'));
+	printf("%s", "\n");
+	printf("%i", ft_tolower('a'));
+	printf("%i", ft_tolower('G'));
+	printf("%i", ft_tolower('0'));
+	printf("%s", "\n");
+	printf("%i", ft_toupper('G'));
+	printf("%i", ft_toupper('a'));
+	printf("%i", ft_toupper('0'));
+	printf("%s", "\n");
+	return (0);
+}
