3d0e82fdb71eab53d907414e7af9108d668e0dd2
mendes-jv <mendes.silva.jv@gmail.com>
2023-05-21 00:18:40 -0300

    refactor(part1): Change islower and isupper to static functions in code

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,11 +6,19 @@
 /*   By: jovicto2 <jovicto2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 19:26:14 by jovicto2          #+#    #+#             */
-/*   Updated: 2023/05/09 19:01:52 by jovicto2         ###   ########.fr       */
+/*   Updated: 2023/05/20 22:41:11 by jovicto2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+static int	ft_islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
+
+static int	ft_isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
 
 int	ft_isalpha(int c)
 {
