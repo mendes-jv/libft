57150355c2696ee9f9c6eb334e84d5bc4e9cd70b
mendes-jv <mendes.silva.jv@gmail.com>
2023-05-28 15:17:39 -0300

    coisei os coiso

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: jovicto2 <jovicto2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 12:23:39 by jovicto2          #+#    #+#             */
-/*   Updated: 2023/05/18 12:53:21 by jovicto2         ###   ########.fr       */
+/*   Updated: 2023/05/28 15:09:18 by jovicto2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	if (!nmemb || !size || !(nmemb != result / size))
 		pointer = malloc(result);
 	if (pointer)
-		ft_memset(pointer, 0, result);
+		ft_bzero(pointer, result);
 	return (pointer);
 }
